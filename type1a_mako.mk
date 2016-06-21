a102c205dc3fdc684fbf9fe081987374d6aca767
kldoc <fred.berbigier@gmail.com>
2016-06-21 06:25:44 -0400

    Make it Type-1a

---
+++ type1a_mako.mk
@@ -0,0 +1,42 @@
+#
+# Copyright 2012 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Sample: This is where we'd set a backup provider if we had one
+# $(call inherit-product, device/sample/products/backup_overlay.mk)
+
+# Inherit from the common Open Source product configuration
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
+
+# Inherit from hardware-specific part of the product configuration
+$(call inherit-product, device/lge/mako/device.mk)
+
+PRODUCT_NAME := type1a_mako
+PRODUCT_DEVICE := mako
+PRODUCT_BRAND := google
+PRODUCT_MODEL := Nexus 4
+PRODUCT_MANUFACTURER := LGE
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+   PRODUCT_NAME=occam BUILD_FINGERPRINT=google/occam/mako:5.1.1/LMY48M/2167285:user/release-keys \
+   PRIVATE_BUILD_DESC="occam-user 5.1.1 LMY48M 2167285 release-keys"
+
+# Kernel inline build
+TARGET_KERNEL_CONFIG := mako_defconfig
+TARGET_KERNEL_CUSTOM_TOOLCHAIN := arm-eabi-4.7
+TARGET_VARIANT_CONFIG := mako_defconfig
+TARGET_SELINUX_CONFIG := mako_defconfig
+
+$(call inherit-product-if-exists, vendor/lge/mako/mako-vendor.mk)
