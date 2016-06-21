5518b3e877506361dd6464008e6a52ae1b4fa919
kldoc <fred.berbigier@gmail.com>
2016-06-21 06:05:18 -0400

    Whitelist device for stlsupport
    Add support for stlport
    
    Based on Cyanogenmod commit
    https://github.com/CyanogenMod/android_device_lge_mako/commit/44040e8774bce749dcecb8f4ad5505f0e1de159d

--- device.mk
+++ device.mk
@@ -41,6 +41,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     charger_res_images
 
+PRODUCT_PACKAGES += \
+    libstlport
+
 ifeq ($(TARGET_PREBUILT_KERNEL),)
 ifeq ($(USE_SVELTE_KERNEL),true)
 LOCAL_KERNEL := device/lge/mako_svelte-kernel/kernel
